b36209cdb7ec973a0c0f3f59c1e843b7ff0a5a86
JonaTheBridge <jonathan@thebridgeschool.es>
2023-01-23 11:29:09 +0100

    add generic router

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ import express from 'express';
 import './dbConnection.js';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import cors from 'cors';
+import apiRouter from './api/router.js';
 import authMiddleware from './middlewares/authMiddleware.js';
 import defaultMiddleware from './middlewares/defaultMiddleware.js';
+
 const server = express();
 const port = process.env.PORT || 3000;
+
 server.use(express.json());
 server.use(cors({ origin: true }));
 server.use(authMiddleware);
 server.use(defaultMiddleware);
+server.use(apiRouter);
 
 server.listen(port, () => {
   console.info(`Server started on port ${port}`);
