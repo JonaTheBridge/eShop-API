4824ccdf2301c61a8899ebac49313f0b196d1bab
JonaTheBridge <jonathan@thebridgeschool.es>
2023-01-23 11:29:38 +0100

    add /docs swagger route

--- src/api/router.js
+++ src/api/router.js
@@ -1,4 +1,8 @@
 import Router from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJson from './swagger.js'; // https://editor.swagger.io
 const router = Router();
+
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 export default router;
