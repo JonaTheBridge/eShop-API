c8a61577c8c3d5c2204d8032b59550ae2bc47e24
JonaTheBridge <jonathan@thebridgeschool.es>
2023-01-23 11:27:03 +0100

    add auth middleware

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import './dbConnection.js';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import cors from 'cors';
+import authMiddleware from './middlewares/authMiddleware.js';
 import defaultMiddleware from './middlewares/defaultMiddleware.js';
 const server = express();
 const port = process.env.PORT || 3000;
 server.use(express.json());
 server.use(cors({ origin: true }));
+server.use(authMiddleware);
 server.use(defaultMiddleware);
 
 server.listen(port, () => {
