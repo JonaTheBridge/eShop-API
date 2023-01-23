631fe7299af36fcfe6834c95482fd5068418671e
JonaTheBridge <jonathan@thebridgeschool.es>
2023-01-23 11:26:39 +0100

    add default middleware

---
+++ src/middlewares/defaultMiddleware.js
@@ -0,0 +1,25 @@
+function middleware(request, response, next) {
+  const { query } = request;
+  console.info(`Middleware listening petition:
+    USER: ${request.user}
+    ROLE: ${request.role}
+    METHOD: ${request.method}
+    URL: ${request.url}
+    QUERY: `, query);
+
+  // Websites to allow
+  response.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods to allow
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+  // Request headers to allow
+  response.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-Access-Token, XKey, Authorization');
+
+  // Request credentials
+  response.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+}
+
+export default middleware;
--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import './dbConnection.js';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import cors from 'cors';
+import defaultMiddleware from './middlewares/defaultMiddleware.js';
 const server = express();
 const port = process.env.PORT || 3000;
 server.use(express.json());
 server.use(cors({ origin: true }));
+server.use(defaultMiddleware);
 
 server.listen(port, () => {
   console.info(`Server started on port ${port}`);
