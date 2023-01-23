f04099527fca14ab051640f8f6c3f2835ff3f673
JonaTheBridge <jonathan@thebridgeschool.es>
2023-01-23 11:26:13 +0100

    add dbConnection

---
+++ src/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { MONGO_DB_NAME, MONGO_URL } = process.env;
+
+const connectionConfig = { dbName: MONGO_DB_NAME, autoIndex: true };
+const connection = mongoose.connect(MONGO_URL, connectionConfig);
+
+if (connection) {
+  console.info('Connection with mongo database successfully');
+} else {
+  console.error('Error to connect with mongo database');
+}
