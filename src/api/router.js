2ab4e4ecf58d9b1027358ee6d65299091d2dc7df
JonaTheBridge <jonathan@thebridgeschool.es>
2023-02-01 16:38:24 +0100

    use usersRouter into global routes

--- src/api/router.js
+++ src/api/router.js
@@ -2,10 +2,14 @@ import Router from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJson from './swagger.js'; // https://editor.swagger.io
 import * as authController from './auth/auth.controller.js';
+import usersRouter from './users/users.routes.js';
+
 const router = Router();
 
 router.post('/login', authController.login);
 router.post('/register', authController.register);
+
+router.use('/users', usersRouter);
 
 router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
