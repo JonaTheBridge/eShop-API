087443d7aade98016590eea3cac107fa2d1dcc46
JonaTheBridge <jonathan@thebridgeschool.es>
2023-02-01 16:32:55 +0100

    complete the users repo

--- src/api/users/users.repository.js
+++ src/api/users/users.repository.js
@@ -1,7 +1,24 @@
 import userModel from './users.model.js';
 
-async function getByUsername({ username }) {
-  const user = await userModel.findOne({ username });
+const defaultOptions = { password: false };
+
+async function getAll() {
+  const users = await userModel.find({}, defaultOptions);
+  return users;
+}
+
+async function getById({ id }) {
+  const user = await userModel.findOne({ _id: id }, defaultOptions);
+  return user;
+}
+
+async function getByUsername({ username }, options = defaultOptions) {
+  const user = await userModel.findOne({ username }, options);
+  return user;
+}
+
+async function getByEmail({ email }) {
+  const user = await userModel.findOne({ email }, defaultOptions);
   return user;
 }
 
@@ -10,4 +27,25 @@ async function insert({ username, email, password }) {
   return user;
 }
 
-export { getByUsername, insert };
+async function update({ id, propsToUpdate }) {
+  const filter = { _id: id };
+  const query = { $set: propsToUpdate };
+  const options = { ...defaultOptions, new: true };
+  const user = await userModel.findOneAndUpdate(filter, query, options);
+  return user;
+}
+
+async function remove({ id }) {
+  const user = await userModel.findOneAndDelete({ _id: id }, defaultOptions);
+  return user;
+}
+
+export {
+  getAll,
+  getById,
+  getByEmail,
+  getByUsername,
+  insert,
+  update,
+  remove,
+};
